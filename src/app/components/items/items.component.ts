fa3ac82329bdef75ebab49910699558689c3aefd
Andres Zapata <azapata@uc.edu.co>
2021-03-24 20:06:52 -0500

    Se agragan componenetes de total y servicio de item

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item} from '../../models/item';
+import {ItemService} from '../../services/item.service'
 
 @Component({
   selector: 'app-items',
@@ -11,31 +12,31 @@ export class ItemsComponent implements OnInit {
 
 
   items: Item[]= [];
+  total: number =0;
 
-  constructor() { }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
 
-        this.items =[
-          {
-          id:0,
-          title:'manzana',
-          price: 10.5,
-          quantity: 4,
-          completed: false
-          },
-          {
-            id:1,
-          title:'pan',
-          price: 3.5,
-          quantity: 8,
-          completed: true
-          }
-
-          
-        ]
-
+        this.items = this.itemService.getItems();
+        this.getTotal(); 
 
   }
+
+  deleteItem(item: Item){
+     this.items = this.items.filter(x=> x.id !== item.id)
+     this.getTotal();
+  }
+
+  toggleItem(item:Item){
+    this.getTotal()
+  }
+
+  getTotal(){
+    this.total = this.items.filter(item=> !item.completed).map(item=>item.quantity*item.price).reduce((acc,item)=> acc +=item,0);
+
+  }
+
+
 
 }
---
+++ src/app/services/item.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {Item} from '../models/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  constructor() { }
+
+  getItems(){
+    return [
+      {
+      id:0,
+      title:'manzana',
+      price: 10.5,
+      quantity: 4,
+      completed: false
+      },
+      {
+        id:1,
+      title:'pan',
+      price: 3.5,
+      quantity: 8,
+      completed: true
+      },
+      {
+        id:2,
+      title:'queso',
+      price: 5,
+      quantity: 2,
+      completed: false
+      }]
+  }
+
+
+
+  addItem(item: Item){
+
+
+  }
+
+
+
+
+}
